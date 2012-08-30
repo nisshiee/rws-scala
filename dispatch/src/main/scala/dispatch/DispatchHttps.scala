4c01cdf34f627e456d2a2fe27cb715514e18dcf1
Hirokazu NISHIOKA <hiro@nisshiee.org>
2012-08-30 13:57:28 +0900

    implement scalaz dispatch

--- dispatch/src/main/scala/dispatch/DispatchHttps.scala
+++ dispatch/src/main/scala/dispatch/DispatchHttps.scala
@@ -2,17 +2,21 @@ package jp.co.rakuten.webservice.dispatch
 
 import jp.co.rakuten.webservice.httptrait._
 import dispatch._
+import com.ning.http.client.{ Response => DRes }
+import scalaz._, Scalaz._
 
 object DispatchHttps extends RwsHttps {
+
+  object AsResponse extends (DRes => Response) {
+    def apply(r: DRes) = Response(r.getStatusCode, r.getResponseBody)
+  }
 
   override def get(
      domain: String
     ,path: String
     ,params: Map[String, String]
-  ): Either[ConnectionError, Response] =
-    Http(:/(domain).secure / path <<? params OK as.String).either.right.map {
-      Response(200, _)
-    }.left.map {
+  ): Validation[ConnectionError, Response] =
+    Http(:/(domain).secure / path <<? params > AsResponse).either.left.map {
       case e: Throwable => ConnectionError(e.getMessage)
-    }()
+    }() |> (_.disjunction.validation)
 }
