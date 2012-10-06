8fcdde2fa7f034dcf09d13a048bc6cfdff95955e
Hirokazu NISHIOKA <hiro@nisshiee.org>
2012-10-06 20:22:40 +0900

    create test of response classes and refactoring

--- core/src/main/scala/webservice/response/ichiba/IchibaItemSearchResultJson.scala
+++ core/src/main/scala/webservice/response/ichiba/IchibaItemSearchResultJson.scala
@@ -1,6 +1,7 @@
 package jp.co.rakuten.webservice
 
 import scalaz._, Scalaz._
+import net.liftweb.json._
 
 case class IchibaItemSearchResultJson (
    count: Int
@@ -13,65 +14,7 @@ case class IchibaItemSearchResultJson (
   ,items: List[ItemJson]
 )
 
-class ItemJson (
-   val itemName: String
-  ,val catchcopy: String
-  ,val itemCode: String
-  ,val itemPrice: String
-  ,val itemCaption: String
-  ,val itemUrl: String
-  ,val availability: Int
-  ,val taxFlag: Int
-  ,val postageFlag: Int
-  ,val creditCardFlag: Int
-  ,val shopName: String
-  ,val shopCode: String
-  ,val shopUrl: String
-  ,val genreId: String
-  ,val imageFlag: Int
-  ,val smallImageUrls: List[String]
-  ,val mediumImageUrls: List[String]
-  ,val affiliateUrl: String
-  ,val shopOfTheYearFlag: Int
-  ,val shipOverseasFlag: Int
-  ,val shipOverseasArea: String
-  ,val asurakuFlag: Int
-  ,val asurakuClosingTime: String
-  ,val asurakuArea: String
-  ,val affiliateRate: String
-  ,val startTime: String
-  ,val endTime: String
-  ,val reviewCount: Int
-  ,val reviewAverage: String
-  ,val pointRate: Int
-  ,val pointRateStartTime: String
-  ,val pointRateEndTime: String
-  ,val giftFlag: Int
-)
-
-object ItemJson {
-
-  import net.liftweb.json._
-
-  lazy val imageUrlExtraction: PartialFunction[JValue, JValue] = {
-    case JObject(JField("imageUrl", jValue) :: Nil) => jValue
-  }
-
-  lazy val itemExtraction: PartialFunction[JValue, JValue] = {
-    case JObject(JField("Item", jValue) :: Nil) => jValue
-  }
-
-  lazy val itemsRename: PartialFunction[JValue, JValue] = {
-    case JField("Items", ja: JArray) => JField("items", ja)
-  }
-
-  lazy val itemTransformation: PartialFunction[JValue, JValue] =
-    imageUrlExtraction orElse itemExtraction orElse itemsRename
-}
-
 object IchibaItemSearchResultJson {
-
-  import net.liftweb.json._
 
   def parseVld(jsonStr: String): Validation[ApiError, IchibaItemSearchResultJson] = {
     implicit val format = DefaultFormats
---
+++ core/src/main/scala/webservice/response/ichiba/ItemJson.scala
@@ -0,0 +1,59 @@
+package jp.co.rakuten.webservice
+
+import scalaz._, Scalaz._
+import net.liftweb.json._
+
+class ItemJson (
+   val itemName: String
+  ,val catchcopy: String
+  ,val itemCode: String
+  ,val itemPrice: String
+  ,val itemCaption: String
+  ,val itemUrl: String
+  ,val availability: Int
+  ,val taxFlag: Int
+  ,val postageFlag: Int
+  ,val creditCardFlag: Int
+  ,val shopName: String
+  ,val shopCode: String
+  ,val shopUrl: String
+  ,val genreId: String
+  ,val imageFlag: Int
+  ,val smallImageUrls: List[String]
+  ,val mediumImageUrls: List[String]
+  ,val affiliateUrl: String
+  ,val shopOfTheYearFlag: Int
+  ,val shipOverseasFlag: Int
+  ,val shipOverseasArea: String
+  ,val asurakuFlag: Int
+  ,val asurakuClosingTime: String
+  ,val asurakuArea: String
+  ,val affiliateRate: String
+  ,val startTime: String
+  ,val endTime: String
+  ,val reviewCount: Int
+  ,val reviewAverage: String
+  ,val pointRate: Int
+  ,val pointRateStartTime: String
+  ,val pointRateEndTime: String
+  ,val giftFlag: Int
+)
+
+object ItemJson {
+
+  lazy val imageUrlExtraction: PartialFunction[JValue, JValue] = {
+    case JObject(JField("imageUrl", (jstr: JString)) :: Nil) => jstr
+  }
+
+  lazy val itemExtraction: PartialFunction[JValue, JValue] = {
+    case JObject(JField("Item", (jo: JObject)) :: Nil) => jo
+  }
+
+  lazy val itemsRename: PartialFunction[JValue, JValue] = {
+    case JField("Items", ja: JArray) => JField("items", ja)
+  }
+
+  lazy val itemTransformation: PartialFunction[JValue, JValue] =
+    imageUrlExtraction orElse itemExtraction orElse itemsRename
+}
+
